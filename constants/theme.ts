8917c268fe593da9e5d84f0208da71d1c495a9f5
Carlos6464 <adrianosodre23@gmail.com>
2026-01-18 19:04:36 -0300

    Initial commit

---
+++ constants/theme.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  background: '#1E1E1E', //
+  surface: '#333333',    //
+  primary: '#F41958',    //
+  textPrimary: '#FFFFFF',//
+  textSecondary: '#8C8C8C',//
+  border: 'rgba(255, 255, 255, 0.1)',
+};
+
+export const SIZES = {
+  padding: 16,
+  radius: 12,
+  inputHeight: 50,
+};
