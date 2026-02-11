f5804a9d15c8a351d5efbf28321582ff7eb2368d
Carlos6464 <adrianosodre23@gmail.com>
2026-02-11 15:00:10 -0300

    Alterando rota nome da rota usuarios

--- app/(auth)/admin/_layout.tsx
+++ app/(auth)/admin/_layout.tsx
@@ -6,16 +6,18 @@ export default function AdminStackLayout() {
             headerStyle: { backgroundColor: '#221015' },
             headerTintColor: '#fff',
             headerTitleStyle: { fontWeight: 'bold' },
-            // CORREÇÃO: Usar headerBackTitle vazia em vez de headerBackTitleVisible
             headerBackTitle: "", 
         }}>
             <Stack.Screen name="index" options={{ title: 'Painel Administrativo' }} />
             
             <Stack.Screen name="posts/index" options={{ title: 'Gerenciar Postagens' }} />
-            <Stack.Screen name="posts/form" options={{ title: 'Editor de Postagem' }} />
+            <Stack.Screen name="posts/form" options={{ title: 'Adicionar/Editar Postagem' }} />
             
             <Stack.Screen name="teachers/index" options={{ title: 'Gerenciar Professores' }} />
             <Stack.Screen name="students/index" options={{ title: 'Gerenciar Alunos' }} />
+
+            <Stack.Screen name="teachers/form" options={{ title: 'Adicionar/Editar Professor' }} />
+            <Stack.Screen name="students/form" options={{ title: 'Adicionar/Editar Aluno' }} />
         </Stack>
     );
 }
